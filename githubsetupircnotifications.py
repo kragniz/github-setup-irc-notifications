a1746483da4e84c004e55ea4d33693a764ac5807
Louis Taylor <louis@kragniz.eu>
2015-11-08 19:17:00 +0000

    Add username and password args

--- githubsetupircnotifications.py
+++ githubsetupircnotifications.py
@@ -3,4 +3,13 @@ github-setup-irc-notifications - Configure all repositories in an organization
 with irc notifications
 """
 
+import argparse
+
 import github3
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username'),
+    parser.add_argument('--password'),
+    args = parser.parse_args()
